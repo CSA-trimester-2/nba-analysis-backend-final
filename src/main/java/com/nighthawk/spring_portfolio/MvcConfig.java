e19b21db875e524402d9719830c7fe64cc80f413
JishnuS420 <111489496+JishnuS420@users.noreply.github.com>
2024-01-19 02:07:40 -0800

    Initial commit

---
+++ src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
@@ -0,0 +1,33 @@
+package com.nighthawk.spring_portfolio;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    // set up your own index
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    /* map path and location for "uploads" outside of application resources
+       ... creates a directory outside "static" folder, "file:volumes/uploads"
+       ... CRITICAL, without this uploaded file will not be loaded/displayed by frontend
+     */
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/volumes/uploads/**").addResourceLocations("file:volumes/uploads/");
+    }
+
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("https://nighthawkcoders.github.io", "http://localhost:4000", "http://127.0.0.1:4100", "https://vardaansinha.github.io", "http://localhost:4200", "http://jishnus420.github.io");
+    }
+    
+}
---
+++ src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
@@ -0,0 +1,105 @@
+package com.nighthawk.spring_portfolio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.nighthawk.spring_portfolio.mvc.jwt.JwtAuthenticationEntryPoint;
+import com.nighthawk.spring_portfolio.mvc.jwt.JwtRequestFilter;
+import com.nighthawk.spring_portfolio.mvc.person.PersonDetailsService;
+
+
+/*
+* To enable HTTP Security in Spring
+*/
+@Configuration
+@EnableWebSecurity  // Beans to enable basic Web security
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+
+	@Autowired
+	private PersonDetailsService personDetailsService;
+
+    // @Bean  // Sets up password encoding style
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		// configure AuthenticationManager so that it knows from where to load
+		// user for matching credentials
+		// Use BCryptPasswordEncoder
+		auth.userDetailsService(personDetailsService).passwordEncoder(passwordEncoder());
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	
+    // Provide security configuration
+		@Bean
+		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+			http
+				.csrf(csrf -> csrf
+					.disable()
+				)
+				// list the requests/endpoints need to be authenticated
+				.authorizeHttpRequests(auth -> auth
+					.requestMatchers("/authenticate").permitAll()
+					.requestMatchers("/mvc/person/update/**", "/mvc/person/delete/**").authenticated()
+					.requestMatchers("/api/person/delete/**").authenticated()
+					.requestMatchers("/**").permitAll()
+				)
+				// support cors
+				.cors(Customizer.withDefaults())
+				.headers(headers -> headers
+					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "true"))
+					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-ExposedHeaders", "*", "Authorization"))
+					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "Content-Type", "Authorization", "x-csrf-token"))
+					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-MaxAge", "600"))
+					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "POST", "PUT", "GET", "OPTIONS", "HEAD"))
+					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "http://localhost:4200", "https://jishnus420.github.io/"))
+				)
+				.formLogin(form -> form 
+					.loginPage("/login")
+				)
+				.logout(logout -> logout
+					.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+					.logoutSuccessUrl("/")
+				)
+				// make sure we use stateless session; 
+				// session won't be used to store user's state.
+				.exceptionHandling(exceptions -> exceptions
+					.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+				)
+				.sessionManagement(session -> session
+					.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				)
+				// Add a filter to validate the tokens with every request
+				.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+			return http.build();
+	}
+}
