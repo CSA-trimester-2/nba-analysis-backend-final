eaa59270e7304795876eb4975dadb49924e69ba5
Pitsco <derrickhuang2@gmail.com>
2024-04-17 22:27:31 -0700

    we love cors errors!

--- src/main/java/com/nighthawk/spring_portfolio/mvc/Statistics/GameController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/Statistics/GameController.java
@@ -1,20 +1,26 @@
 package com.nighthawk.spring_portfolio.mvc.Statistics;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.*;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 
-@Controller
+@RestController
 @RequestMapping("/games")
-@CrossOrigin(origins = "*") // Allow requests from all origins
+@CrossOrigin(origins = {"http://127.0.0.1:4000"})
 public class GameController {
 
     private static final String API_URL = "https://api.balldontlie.io/v1/stats";
     private static final String API_KEY = "f8073004-09ea-475c-840b-380354e78ae5";
 
-    @GetMapping("/{playerId}/{season}") // Adjusted the mapping to remove redundant "/games"
+    @GetMapping("/{playerId}/{season}")
     @ResponseBody
     public ResponseEntity<String> getPlayerGames(
             @PathVariable("playerId") int playerId,
@@ -25,7 +31,7 @@ public class GameController {
 
         // Create headers with API key
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + API_KEY);
+        headers.set("Authorization", API_KEY);
 
         // Make a GET request to the API
         HttpEntity<String> entity = new HttpEntity<>(headers);
