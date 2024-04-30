8922916fe459c8d42ae78449e705c6f3b064ab96
VishnuAravind12 <vishnuaa77@gmail.com>
2024-04-30 13:41:19 -0700

    merged person

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
@@ -35,34 +35,28 @@ import lombok.NonNull;
 
 /*
 Person is a POJO, Plain Old Java Object.
-First set of annotations add functionality to POJO
---- @Setter @Getter @ToString @NoArgsConstructor @RequiredArgsConstructor
-The last annotation connect to database
---- @Entity
- */
+Annotations add functionality to POJO and connect it to the database.
+*/
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Convert(attributeName ="person", converter = JsonType.class)
+@Convert(attributeName = "person", converter = JsonType.class)
 public class Person {
 
-    // automatic unique identifier for Person record
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    // email, password, roles are key attributes to login and authentication
     @NotEmpty
-    @Size(min=5)
-    @Column(unique=true)
+    @Size(min = 5)
+    @Column(unique = true)
     @Email
     private String email;
 
     @NotEmpty
     private String password;
 
-    // @NonNull, etc placed in params of constructor: "@NonNull @Size(min = 2, max = 30, message = "Name (2 to 30 chars)") String name"
     @NonNull
     @Size(min = 2, max = 30, message = "Name (2 to 30 chars)")
     private String name;
@@ -70,27 +64,21 @@ public class Person {
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dob;
 
-    // To be implemented
     @ManyToMany(fetch = EAGER)
     private Collection<PersonRole> roles = new ArrayList<>();
 
-    /* HashMap is used to store JSON for daily "stats"
-    "stats": {
-        "2022-11-13": {
-            "calories": 2200,
-            "steps": 8000
-        }
-    }
-    */
     @JdbcTypeCode(SqlTypes.JSON)
     @Column(columnDefinition = "jsonb")
-    private Map<String,Map<String, Object>> stats = new HashMap<>(); 
-    
+    private Map<String, Map<String, Object>> stats = new HashMap<>();
+
+    private Integer eco;
+    private String primaryCrop;
+    private Integer cash;
+
     @JdbcTypeCode(SqlTypes.JSON)
     @Column(columnDefinition = "jsonb")
     private Map<String, Integer> integerMap = new HashMap<>();
 
-    // Constructor used when building object from an API
     public Person(String email, String password, String name, Date dob) {
         this.email = email;
         this.password = password;
@@ -98,101 +86,39 @@ public class Person {
         this.dob = dob;
     }
 
-    // A custom getter to return age from dob attribute
+    public Person(String email, String password, String name, Integer eco, String primaryCrop, Integer cash, Date dob) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.eco = eco;
+        this.primaryCrop = primaryCrop;
+        this.cash = cash;
+        this.dob = dob;
+    }
+
     public int getAge() {
         if (this.dob != null) {
             LocalDate birthDay = this.dob.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-            return Period.between(birthDay, LocalDate.now()).getYears(); }
+            return Period.between(birthDay, LocalDate.now()).getYears();
+        }
         return -1;
     }
 
-    // Initialize static test data 
     public static Person[] init() {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        Person[] persons = new Person[2];
 
-        // basics of class construction
-        Person p1 = new Person();
-        p1.setName("Thomas Edison");
-        p1.setEmail("toby@gmail.com");
-        p1.setPassword("123Toby!");
-        p1.getIntegerMap().put("Team 1", 1);
-        p1.getIntegerMap().put("Team 2", 40);
-        // adding Note to notes collection
-        try {  // All data that converts formats could fail
-            Date d = new SimpleDateFormat("MM-dd-yyyy").parse("01-01-1840");
-            p1.setDob(d);
-        } catch (Exception e) {
-            // no actions as dob default is good enough
-        }
-
-        Person p2 = new Person();
-        p2.setName("Tester Testing");
-        p2.setEmail("test@gmail.com");
-        p2.setPassword("password");
-        p2.getIntegerMap().put("Team 3", 2);
-        p2.getIntegerMap().put("Team 4", 100);
         try {
-            Date d = new SimpleDateFormat("MM-dd-yyyy").parse("01-01-1845");
-            p2.setDob(d);
+            persons[0] = new Person("mirzahbeg123@gmail.com", "notTellingYouLOL", "h4seebcmd", -5, "corn", 52, sdf.parse("12-06-2007"));
+            persons[1] = new Person("ermitsactuallypronouncedwithaTHUH@gmail.com", "iLOVEagricutlre", "tirthFarmer999", 8, "corn", 14, sdf.parse("01-01-2024"));
         } catch (Exception e) {
         }
 
-        Person p3 = new Person();
-        p3.setName("Nikola Tesla");
-        p3.setEmail("niko@gmail.com");
-        p3.setPassword("123Niko!");
-        p3.getIntegerMap().put("Team 6", 60);
-        p3.getIntegerMap().put("Team 5", 30);
-        try {
-            Date d = new SimpleDateFormat("MM-dd-yyyy").parse("01-01-1850");
-            p3.setDob(d);
-        } catch (Exception e) {
-        }
-
-        Person p4 = new Person();
-        p4.setName("Madam Currie");
-        p4.setEmail("madam@gmail.com");
-        p4.setPassword("123Madam!");
-        p4.getIntegerMap().put("Team 8", 120);
-        p4.getIntegerMap().put("Team 9", 203);
-        try {
-            Date d = new SimpleDateFormat("MM-dd-yyyy").parse("01-01-1860");
-            p4.setDob(d);
-        } catch (Exception e) {
-        }
-
-        Person p5 = new Person();
-        p5.setName("John Mortensen");
-        p5.setEmail("jm1021@gmail.com");
-        p5.setPassword("123Qwerty!");
-        p5.getIntegerMap().put("Team 91", 808);
-        p5.getIntegerMap().put("Easter Egg", 90210);
-        try {
-            Date d = new SimpleDateFormat("MM-dd-yyyy").parse("10-21-1959");
-            p5.setDob(d);
-        } catch (Exception e) {
-        }
-
-        // Array definition and data initialization
-        Person persons[] = {p1, p2, p3, p4, p5};
-        return(persons);
-    }
-
-    public static void initializeIntegerMaps(Person[] persons) {
-            for (Person person : persons) {
-                Map<String, Integer> integerMap = person.getIntegerMap();
-                if (integerMap.isEmpty()) {
-                    // Initialize integerMap with different values for each person
-                    integerMap.put("value1", 10);
-                    integerMap.put("value2", 20);
-                    integerMap.put("value3", 30);
-                }
-            }
-    
+        return persons;
     }
 
     public static void main(String[] args) {
         Person[] persons = init();
-        initializeIntegerMaps(persons);
         for (Person person : persons) {
             System.out.println("Name: " + person.getName());
             System.out.println("Email: " + person.getEmail());
@@ -200,6 +126,9 @@ public class Person {
             System.out.println("Date of Birth: " + person.getDob());
             System.out.println("Age: " + person.getAge());
             System.out.println("Integer Map: " + person.getIntegerMap());
+            System.out.println("Eco: " + person.getEco());
+            System.out.println("Primary Crop: " + person.getPrimaryCrop());
+            System.out.println("Cash: " + person.getCash());
             System.out.println("---------------------------------");
         }
     }
